1020732e627838b508d25961fc7b96f6483e7d34
William Estrela Louzeiro <wel.estrela@gmail.com>
2019-04-06 11:31:25 -0300

    Implementação do modelo para persistência

--- src/model/Celular.java
+++ src/model/Celular.java
@@ -1,5 +1,63 @@
 package model;
 
-public class Celular {
+import java.util.List;
 
+import interfaces.IConversor;
+import util.Retorno;
+
+public class Celular extends Modelo<Integer>{
+	
+	private String imei;
+	private String modelo;
+	private String cor;
+	private String ano;
+	
+	
+	@Override
+	public String getModeloNome() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public String getModeloPKNome() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public List<Object> getCamposValor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public boolean getUsarPkNaInsercao() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public List<IConversor> getCamposConversor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public List<String> getCamposNome() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	protected List<String> getCamposObrigatorios() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	protected Retorno setCamposValor(List<Object> list) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public Modelo<?> getNovoObjeto() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
 }
--- src/model/Proprietario.java
+++ src/model/Proprietario.java
@@ -1,5 +1,63 @@
 package model;
 
-public class Proprietario {
+import java.util.List;
+
+import interfaces.IConversor;
+import util.Retorno;
+
+public class Proprietario extends Modelo<Integer>{
+	
+	private String nome;
+	private String cpf;
+	private String telefone;
+	
+	private Celular celular;
+	
+	
+	@Override
+	public String getModeloNome() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public String getModeloPKNome() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public List<Object> getCamposValor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public boolean getUsarPkNaInsercao() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public List<IConversor> getCamposConversor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public List<String> getCamposNome() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	protected List<String> getCamposObrigatorios() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	protected Retorno setCamposValor(List<Object> list) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public Modelo<?> getNovoObjeto() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
