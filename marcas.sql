d37aa3cd9293f8ec0a54db951f041f92e64ae920
William Estrela Louzeiro <wel.estrela@gmail.com>
2019-04-26 20:07:49 -0300

    Arquivo de marcas

---
+++ marcas.sql
@@ -0,0 +1,5 @@
+INSERT INTO marca (id, nome) VALUES (1, 'samsung');
+INSERT INTO marca (id, nome) VALUES (2, 'apple');
+INSERT INTO marca (id, nome) VALUES (3, 'motorola');
+INSERT INTO marca (id, nome) VALUES (4, 'lenovo');
+INSERT INTO marca (id, nome) VALUES (5, 'xiaomi');
