bae45ffbc75f637a27e4e2727966eb04061c2fa5
William Estrela Louzeiro <wel.estrela@gmail.com>
2019-06-05 10:37:32 -0300

    Troca do sistema proprietário para o sistema de listar com mini templator

--- src/view/CadastroServelet.java
+++ src/view/CadastroServelet.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import controller.ControladorManterCelular;
 import model.Celular;
 import model.Marca;
 import persistence.DAOCelular;
--- src/view/ListarServelet.java
+++ src/view/ListarServelet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import biz.source_code.miniTemplator.MiniTemplator;
 import controller.ControladorManterCelular;
 import model.Celular;
 import util.FileToString;
@@ -45,10 +46,8 @@ public class ListarServelet extends HttpServlet{
 			flag = 1;
 		}
 
-		
 		PrintWriter out = response.getWriter();
 		response.setContentType( "text/html" );
-		
 		
 		if(flag == 1) {//procurando um celular em especifico
 			celulares = repositoryCelular.ListarCelular(imeiAprocurar);
@@ -59,47 +58,31 @@ public class ListarServelet extends HttpServlet{
 		}
 		
 		String fileSeparator = System.getProperty("file.separator");
-		String listarCelular = FileToString.convert(this.getServletContext().getRealPath(fileSeparator)+fileSeparator+"WEB-INF"+fileSeparator+"classes"+fileSeparator+"view"+fileSeparator+ "listarCelular.html");
-		
-		
-		String nova = new String();
-		
+		String localArquivoListar = this.getServletContext().getRealPath(fileSeparator)+fileSeparator+"WEB-INF"+fileSeparator+"classes"+fileSeparator+"view"+fileSeparator+ "listarCelular.html"; 
+
+		MiniTemplator t = new MiniTemplator(localArquivoListar);
+
 		int tamanho = celulares.size();
-		
 		for(int i=0; i < tamanho; i++) {
+
+			t.setVariable("indice", i);
+			t.setVariable("imei", celulares.get(i).getImei());
+			t.setVariable("marca", celulares.get(i).getMarca());
+			t.setVariable("modelo", celulares.get(i).getModelo());
+			t.setVariable("cor", celulares.get(i).getCor());
+			t.setVariable("ano", celulares.get(i).getAno());
+			t.setVariable("id", celulares.get(i).getId());
 			
-			String temporaria = "<td>##</td> \n";
-			int id = celulares.get(i).getId();
-			
-			nova += "<tr>";
-			nova += temporaria.replaceAll("<td>##</td>", "<td>" + i + "</td>");
-			nova += temporaria.replaceAll("<td>##</td>", "<td>" + celulares.get(i).getImei() + "</td>");
-			nova += temporaria.replaceAll("<td>##</td>", "<td>" + celulares.get(i).getMarca() + "</td>");
-			nova += temporaria.replaceAll("<td>##</td>", "<td>" + celulares.get(i).getModelo() + "</td>");
-			nova += temporaria.replaceAll("<td>##</td>", "<td>" + celulares.get(i).getCor() + "</td>");
-			nova += temporaria.replaceAll("<td>##</td>", "<td>" + celulares.get(i).getAno() + "</td>");
-			nova += temporaria.replaceAll("<td>##</td>", "<td>"
-					+ " <a href=\"remover?id="+ id +"\" formmethod=\"post\" class=\"btn btn-danger btn-sm\"><span class=\\\"fa fa-minus\\\"></span></button> \n"
-					+ " <a href=\"update?id="+ id +"\" formmethod=\"post\" class=\"btn btn-info btn-sm\"><span class=\\\"fa fa-refresh\\\"></span></button> \n"
-					+ " </td>");
-			nova += "</tr>";
-			
-			
+			t.addBlock("mantercelular");
+
 		}
 		
-		String formularioFinal = listarCelular.replaceAll("##</td>", nova);
-		
-		String cadastroCelular = FileToString.convert(this.getServletContext().getRealPath(fileSeparator)+fileSeparator+"WEB-INF"+fileSeparator+"classes"+fileSeparator+"view"+fileSeparator+ "cadastroCelular.html");
-		String teste = new String();
-		teste = formularioFinal.replaceAll("@@", cadastroCelular);
-		
 		if(celulares.isEmpty()) {
-			
 			out.print("<script>alert(\"Nada encontrado\")</script>");
 			response.sendRedirect("cadastro");
 		}
 		
-		out.print(teste);
+		out.print(t.generateOutput());
 	}
 
 }
